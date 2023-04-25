45100298a98a6474ac1365d01b33463e6892bd95
ann-bereza <annhavelia@gmail.com>
2023-04-25 19:17:31 +0300

    first homework

--- DjangoCourseHW/lesson1/views.py
+++ DjangoCourseHW/lesson1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>This is a Homepage</h1>")
+    return render(request, "lesson1/hello.html")
 
 
 def les_1_1(request):
