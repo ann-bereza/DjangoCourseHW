661b9c5e06050d8cac8ebcbdc96e7ca70030dab0
ann-bereza <annhavelia@gmail.com>
2023-04-25 18:47:15 +0300

    Revert "initial commit"
    This reverts commit 3facc22811d8619485ac332385eeb126913570a2.

--- DjangoDevCourse/lesson11/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from lesson11 import views
-
-urlpatterns = [
-    path('lesson_1/', views.index, name="home"),
-    path('lesson_1_1/', views.les_1_1, name="lesson_1_1"),
-
-]
