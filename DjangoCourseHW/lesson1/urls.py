8b0c52022cc2af85722fc38d5a513045a5e002be
ann-bereza <annhavelia@gmail.com>
2023-04-25 19:21:04 +0300

    first homework

--- DjangoCourseHW/lesson1/urls.py
+++ DjangoCourseHW/lesson1/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from lesson1 import views
 
 urlpatterns = [
-    path('lesson_1/', views.index, name="home"),
+    path('', views.index, name="home"),
+    path('lesson_1/', views.les_1, name="lesson_1"),
     path('lesson_1_1/', views.les_1_1, name="lesson_1_1"),
 
 ]
--- DjangoCourseHW/lesson1/views.py
+++ DjangoCourseHW/lesson1/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 
 
 def index(request):
+    return HttpResponse("<h1>This is a Homepage</h1>")
+
+
+def les_1(request):
     return render(request, "lesson1/hello.html")
 
 
 def les_1_1(request):
-    return render(request, "lesson1/index.html")
+    return render(request, "lesson1/hello_2.html")
